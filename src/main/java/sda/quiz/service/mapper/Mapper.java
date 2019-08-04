8d6b8a307e959c4f1061a97a38f611a3f2d07db5
afornalik <afornalik2@gmail.com>
2019-08-04 15:13:09 +0200

    Work with mapper and service.

--- src/main/java/sda/quiz/service/mapper/Mapper.java
+++ src/main/java/sda/quiz/service/mapper/Mapper.java
@@ -1,8 +1,11 @@
 package sda.quiz.service.mapper;
 
+import sda.quiz.service.mapper.exception.ConvertDtoToEntityException;
+import sda.quiz.service.mapper.exception.ConvertEntityToDtoException;
+
 public interface Mapper<E,D> {
 
-    E convertDtoToEntity(D d);
+    E convertDtoToEntity(D d) throws ConvertDtoToEntityException;
 
-    D convertEntityToDto(E e);
+    D convertEntityToDto(E e) throws ConvertEntityToDtoException;
 }
