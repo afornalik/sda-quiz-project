d9d361e9ce8f07940a3010ff100c3b5612851f5d
Mariusz Zaprzala <zmariusz@op.pl>
2019-08-11 14:16:09 +0200

    zmiana wyglądu, dodanie fragmentow stron, wyswietla liste question

--- src/main/java/sda/quiz/controller/LoginController.java
+++ src/main/java/sda/quiz/controller/LoginController.java
@@ -7,14 +7,11 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import sda.quiz.dto.QuestionDto;
 import sda.quiz.entity.User;
 import sda.quiz.service.QuestionService;
-import sda.quiz.service.QuizService;
 import sda.quiz.service.UserService;
 
 
@@ -90,43 +87,14 @@ public class LoginController {
 //        ModelAndView modelAndView = new ModelAndView();
 //        return modelAndView;
 //    }
-    @RequestMapping(value = "/quiz",method = RequestMethod.GET)
-    public ModelAndView quiz(){
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("quiz");
-        System.out.println("sdfsdfsdf");
-
-        return modelAndView;
-    }
-    @RequestMapping(value = "/showQuestions",method = RequestMethod.GET)
-    public ModelAndView showAllQuestions() {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("questions",questionService.getAllQuestions());
-        modelAndView.setViewName("question/showAllQuestion");
-        return modelAndView;
-    }
-
-    @RequestMapping(value = "/addQuestion",method = RequestMethod.GET)
-    public ModelAndView addQuestionEmptyForm(){
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("question",questionService.createEmptyQuestionWith4Answer());
-        modelAndView.setViewName("question/addQuestionForm");
-        return modelAndView;
-    }
-
-
-    @RequestMapping(value = "/addQuestion",method = RequestMethod.POST)
-    public ModelAndView addQuestionAfterInsertForm(@ModelAttribute ("question") QuestionDto questionDto) {
-        ModelAndView modelAndView = new ModelAndView();
-        try {
-            questionService.saveNewQuestion(questionDto);
-            modelAndView.setViewName("question/addQuestionConfirm");
-        } catch (Exception e) {
-            modelAndView.addObject("error", e.getMessage());
-            modelAndView.setViewName("question/addQuestionForm");
-        }
-        return modelAndView;
-    }
+//    @RequestMapping(value = "/quiz",method = RequestMethod.GET)
+//    public ModelAndView quiz(){
+//        ModelAndView modelAndView = new ModelAndView();
+//        modelAndView.setViewName("quiz");
+//        System.out.println("sdfsdfsdf");
+//
+//        return modelAndView;
+//    }
 
 
 
