771a977883b4d5d2c8f764b4d854338370054f8d
Andrzejf <afornalik2@gmail.com>
2019-08-26 00:43:15 +0200

    clean up #2 LoggedController

--- src/main/java/sda/quiz/controller/LoggedController.java
+++ src/main/java/sda/quiz/controller/LoggedController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,43 +17,33 @@ import sda.quiz.service.implementation.UserService;
 @Controller
 public class LoggedController {
 
-    private final UserService userService;
     private final IQuizService quizService;
 
     @Autowired
-    public LoggedController(UserService userService, IQuizService quizService) {
-        this.userService = userService;
+    public LoggedController(IQuizService quizService) {
         this.quizService = quizService;
     }
 
+    @RequestMapping(value = "/admin/home")
+    public ModelAndView home(@RequestParam(name = "delete", required = false) Long quizToDelete,
+                             @RequestParam(name = "run", required = false) Long quizToRun,
+                             @RequestParam(name = "category", required = false) String category) {
 
-    @RequestMapping(value = "/admin/home", method = RequestMethod.GET)
-    public ModelAndView home(@RequestParam(name = "delete",required = false) Long quizToDelete,
-                             @RequestParam(name = "run",required = false) Long quizToRun,
-                             @RequestParam(name ="category",required = false)String category) {
+        ModelAndView modelAndView = new ModelAndView("admin/home");
 
-        ModelAndView modelAndView = new ModelAndView();
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        User user = userService.findUserByEmail(auth.getName());
-
-        if(category != null && !category.equals("wszystkie")) {
-            modelAndView.addObject("quizList",quizService.getQuizzes(Category.valueOf(category)));
-        }else if(quizToDelete != null) {
-            quizService.deleteQuiz(quizToDelete);
-            modelAndView.addObject("quizList",quizService.getAllQuiz());
-        } else if(quizToRun != null) {
-            QuizDto quizDto = quizService.getQuizById(quizToRun,true);
-            modelAndView.addObject("quiz",quizDto);
-            modelAndView.setViewName("admin/quiz/runQuiz");
+        if (category != null && !category.equals("all")) {
+            modelAndView.addObject("quizList", quizService.getQuizzes(Category.valueOf(category)));
             return modelAndView;
-        }else{
-            modelAndView.addObject("quizList",quizService.getAllQuiz());
+
+        } else if (quizToRun != null) {
+            modelAndView.addObject("quizToRun", quizService.getQuizById(quizToRun, true));
+            modelAndView.setViewName("admin/quiz/runQuiz");
+
+        } else if (quizToDelete != null) {
+            quizService.deleteQuiz(quizToDelete);
         }
 
-
-        modelAndView.addObject("userName", "Witam na naszej stronie " + user.getName());
-        modelAndView.addObject("adminMessage","Content available only for users with admin role");
-        modelAndView.setViewName("admin/home");
+        modelAndView.addObject("quizList", quizService.getAllQuiz());
         return modelAndView;
     }
 }
